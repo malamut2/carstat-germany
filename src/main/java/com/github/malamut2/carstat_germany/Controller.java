c8fb56325a303b8f411076aa2306fa7ce530b796
Wolfgang Kronberg <wolfgang.kronberg@gmail.com>
2021-07-14 20:00:04 +0200

    Prepare data retrieval mapping

--- src/main/java/com/github/malamut2/carstat_germany/Controller.java
+++ src/main/java/com/github/malamut2/carstat_germany/Controller.java
@@ -1,6 +1,7 @@
 package com.github.malamut2.carstat_germany;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class Controller {
     @GetMapping("/hello")
     public String hello() {
         return "Hello!";
+    }
+
+    @PostMapping("/retrieveFromKBA")
+    public void retrieveStatisticsDataFromKBA() {
+
     }
 
 }
